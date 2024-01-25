e8d0b44fd51c62a84a8c1f5b4acfa995401bd03c
S H-SuHan <suhanur35-448@diu.edu.bd>
2024-01-26 01:34:05 +0600

    Product Calculator section here

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@
 import Banner from "@/components/ui/Banner";
 import BestCourier from "@/components/ui/BestCourier";
 import CallUs from "@/components/ui/CallUs";
+import DeliveryCalculator from "@/components/ui/DeliveryCalculator/DeliveryCalculator";
 import FeaturesSection from "@/components/ui/FeaturesSection";
 import OurServices from "@/components/ui/OurServices";
 import Parcel from "@/components/ui/Parcel";
@@ -20,6 +21,7 @@ const page = () => {
       <OurServices />
       <ProductDelivery />
       <BestCourier></BestCourier>
+      <DeliveryCalculator></DeliveryCalculator>
       <CallUs></CallUs>
       <FeaturesSection></FeaturesSection>
       <Stat></Stat>

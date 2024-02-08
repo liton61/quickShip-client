09bdecf1f772aeeca1b408fb79a9daeae9a6964c
liton61 <litonofficial61@gmail.com>
2024-02-08 20:47:15 +0600

    get method added for all users

--- src/app/dashboard/usersManage/page.js
+++ src/app/dashboard/usersManage/page.js
@@ -1,59 +1,106 @@
-// /* eslint-disable react-hooks/rules-of-hooks */
-// "use client"
-// import usePublicAxios from '@/components/hooks/usePublicAxios';
-// import { useQuery } from '@tanstack/react-query';
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import usePublicAxios from '@/components/hooks/usePublicAxios';
+import { useQuery } from '@tanstack/react-query';
+import { FaTrash, FaUser } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
-// const page = () => {
-//     const axiosPublic = usePublicAxios();
-//     const { data: user = [], refetch } = useQuery({
-//         queryKey: ['user'],
-//         queryFn: async () => {
-//             const res = await axiosPublic.get('/users');
-//             return res.data;
-//         }
-//     })
-//     return (
-//         <div className="lg:w-3/4 mx-auto lg:px-0 px-5 mt-10">
-//             <div className="stats shadow flex justify-center text-center">
-//                 <div className="stat">
-//                     <div className="stat-title">Total Users</div>
-//                     <div className="stat-value">{user.length}</div>
-//                 </div>
-//             </div>
-//             <div className="overflow-x-auto mt-10">
-//                 <table className="table w-full">
-//                     {/* head */}
-//                     <thead className="bg-blue-500 text-white">
-//                         <tr>
-//                             <th>#</th>
-//                             <th>Name</th>
-//                             <th>Email</th>
-//                             {/* <th>Role</th> */}
-//                             <th>Action</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {
-//                             user.map((user, index) => <tr key={user._id}>
-//                                 <th>{index + 1}</th>
-//                                 <td>{user.name}</td>
-//                                 <td>{user.email}</td>
-//                                 <td>Delete</td>
-//                                 {/* <td>
-//                                     {user.role === 'admin' ? 'Admin' : <button
-//                                         onClick={() => handleMakeAdmin(user)}>
-//                                         <i className="fa-solid fa-users text-xl text-green-600"></i>
-//                                     </button>}
-//                                 </td>
-//                                 <button onClick={() => handleDeleteUser(user)}><i className="fa-solid fa-trash-can text-xl text-red-600 mt-3"></i></button> */}
-//                             </tr>)
-//                         }
+const page = () => {
+    const axiosPublic = usePublicAxios();
+    const { data: user = [], refetch } = useQuery({
+        queryKey: ['user'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/users');
+            return res.data;
+        }
+    })
 
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </div>
-//     );
-// };
+    // const handleMakeAdmin = user => {
+    //     axiosPublic.patch(`/user/admin/${user._id}`)
+    //         .then(res => {
+    //             console.log(res.data)
+    //             if (res.data.modifiedCount > 0) {
+    //                 refetch();
+    //                 Swal.fire({
+    //                     position: "top",
+    //                     icon: "success",
+    //                     title: `${user.name} is an Admin Now!`,
+    //                     showConfirmButton: false,
+    //                     timer: 1500
+    //                 });
+    //             }
+    //         })
+    // }
 
-// export default page;
+    // const handleDeleteUser = user => {
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "You won't be able to revert this!",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, delete it!"
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             axiosPublic.delete(`/user/${user._id}`)
+    //                 .then(res => {
+    //                     // console.log(res.data);
+    //                     if (res.data.deletedCount > 0) {
+    //                         Swal.fire({
+    //                             title: "Deleted!",
+    //                             text: "Your file has been deleted.",
+    //                             icon: "success"
+    //                         });
+    //                         refetch()
+    //                     }
+    //                 })
+    //         }
+    //     });
+    // }
+    return (
+        <div className="lg:w-3/4 mx-auto lg:px-0 px-5 mt-10">
+            <div className="stats shadow flex justify-center text-center">
+                <div className="stat">
+                    <div className="stat-title">Total Users</div>
+                    <div className="stat-value">{user.length}</div>
+                </div>
+            </div>
+            <div className="overflow-x-auto mt-10">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-blue-500 text-white">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            user.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td className='cursor-pointer'>{user.role}</td>
+                                <td className='text-red-600 cursor-pointer'><FaTrash></FaTrash></td>
+                                {/* <td>
+                                    {user.role === 'admin' ? 'Admin' : <button
+                                        onClick={() => handleMakeAdmin(user)}>
+                                        <FaUser className="fa-solid fa-users text-xl text-green-600"></FaUser>
+                                    </button>}
+                                </td>
+                                <button onClick={() => handleDeleteUser(user)}><FaTrash className="fa-solid fa-trash-can text-lg text-red-600 mt-3"></FaTrash></button> */}
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default page;

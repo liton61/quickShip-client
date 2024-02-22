e2dea8fc208c53123b42d029572f0ec52044d2e0
saha566789 <tomasaha3456@gmail.com>
2024-02-22 22:44:24 +0600

    notification page designed

--- src/components/ui/Notifications.js
+++ src/components/ui/Notifications.js
@@ -2,28 +2,34 @@ import { IoIosNotifications } from "react-icons/io";
 import useOrder from "../hooks/useOrder";
 import usePayment from "../hooks/usePayment";
 import useApplication from "../hooks/useApplication";
+import { MdNotificationsActive } from "react-icons/md";
 
 
 const Notifications = () => {
-    const [order,refetch] = useOrder()
+    const [order, refetch] = useOrder()
     const [payment] = usePayment()
-   const [application] = useApplication()
-   refetch()
+    const [application] = useApplication()
+    refetch()
     return (
         <div>
-             
-            { order.length && payment.length && application.length === 0? <IoIosNotifications className="text-blue-600 text-4xl relative" />: <details className="dropdown  dropdown-end">
+
+            {order.length && payment.length && application.length === 0 ? <IoIosNotifications className="text-blue-600 text-4xl relative" /> : <details className="dropdown  dropdown-end">
                 <summary className="m-1 btn btn-ghost"><IoIosNotifications className="text-blue-600 text-4xl relative" />
                     {<span className="text-white absolute top-2 right-6">{order.length + payment.length + application.length}</span>}
                 </summary>
+
                 <ul className="p-2 w-96 shadow menu dropdown-content z-[1] bg-base-100 rounded-box max-h-[700px] grid grid-cols-1 overflow-y-scroll">
-                   {order?.map((item)=><li key={item._id} className="p-2 text-lg text-center shadow block">Your product is {item.status}</li>)}
-                   {payment?.map((item)=><li key={item._id} className="p-2 text-lg text-center shadow block">Your payment are {item.payment} done</li>)}
-                   {application?.map((item)=><li key={item._id} className="p-2 text-lg text-center shadow block">{item.role === 'user'? " your  application successfully applied " :" You become a delivery Boy"}</li>)}
-                 
+
+                    <h1 className="text-xl font-bold">Notifications</h1>
+                    {order?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg shadow block">Your product is {item.status}</li></div>))}
+
+                    {payment?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg  shadow block">Your payment are {item.payment} done</li></div>))}
+
+                    {application?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg  shadow block">{item.role === 'user' ? " your  application successfully applied " : " You become a delivery Boy"}</li></div>))}
+
                 </ul>
             </details>}
-             
+
         </div>
     );
 };

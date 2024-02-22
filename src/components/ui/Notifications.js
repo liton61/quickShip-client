ff3cb70c4e9d13d767eff02fa1e3eca795dce607
saha566789 <tomasaha3456@gmail.com>
2024-02-22 22:51:49 +0600

    notification responsive

--- src/components/ui/Notifications.js
+++ src/components/ui/Notifications.js
@@ -18,14 +18,14 @@ const Notifications = () => {
                     {<span className="text-white absolute top-2 right-6">{order.length + payment.length + application.length}</span>}
                 </summary>
 
-                <ul className="p-2 w-96 shadow menu dropdown-content z-[1] bg-base-100 rounded-box max-h-[700px] grid grid-cols-1 overflow-y-scroll">
+                <ul className="p-2  w-64 lg:w-96 shadow menu dropdown-content z-[1] bg-base-100 rounded-box max-h-[700px] grid grid-cols-1 overflow-y-scroll">
 
                     <h1 className="text-xl font-bold">Notifications</h1>
-                    {order?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg shadow block">Your product is {item.status}</li></div>))}
+                    {order?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2  text-xs lg:text-lg shadow block">Your product is {item.status}</li></div>))}
 
-                    {payment?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg  shadow block">Your payment are {item.payment} done</li></div>))}
+                    {payment?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-xs lg:text-lg shadow block">Your payment are {item.payment} done</li></div>))}
 
-                    {application?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-lg  shadow block">{item.role === 'user' ? " your  application successfully applied " : " You become a delivery Boy"}</li></div>))}
+                    {application?.map((item) => (<div key={item._id} className="flex justify-start items-center"><MdNotificationsActive className="text-xl" /><li className="p-2 text-xs lg:text-lg  shadow block">{item.role === 'user' ? " your  application successfully applied " : " You become a delivery Boy"}</li></div>))}
 
                 </ul>
             </details>}
